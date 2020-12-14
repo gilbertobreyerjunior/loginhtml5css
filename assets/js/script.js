b71c29c77b33222bf6aeac7efcae1154b36ca4e3
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2020-12-13 23:16:20 -0300

    Adicionando o inicio da validacao JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
 console.log('Teste');
+
+
+
+document.querySelector('#btn-submit').addEventListener('click', e =>{
+
+console.login('O botão foi clicado');
+
+
+});
+
+
+
+
+let email = querySelector('#email').value;
+let password = querySelector('#password').value;
+
+
+console.log(email, password);
+
+
+let json = {
+
+
+email,
+password
+
+
+
+
+
+}
